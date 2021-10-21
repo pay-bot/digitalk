062ccc78dc5677c4df282a47764a7b1879f994d7
unknown <afahri22@yahoo.com>
2021-10-21 23:02:18 +0700

    rmrf

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
 			<ul className='hidden sm:flex flex-1 justify-end items-center gap-12 text-landing-blue uppercase text-xs'>
 				<li className='cursor-pointer'>Features</li>
 				<li className='cursor-pointer'>Pricing</li>
-				<li className='cursor-pointer'>Contact</li>
 				<a href="https://wa.me/08984629140" className="bg-green-300 px-8 py-2 rounded-xl">Hubungi</a>
 			</ul>
 			<div className='flex sm:hidden flex-1 justify-end'>
